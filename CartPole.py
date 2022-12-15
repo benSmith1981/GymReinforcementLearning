1e26c6c1968bb69eaa46f6d8bdcee53058af76c9
Ben Smith <benjaminsmith1981@googlemail.com>
2022-12-15 11:08:02 +0000

    First

---
+++ CartPole.py
@@ -0,0 +1,40 @@
+import gym
+import os
+os.environ['SDL_VIDEODRIVER']='windib'
+env = gym.make("CartPole-v1", render_mode="rgb_array")
+env.reset()
+env.render()
+
+
+# import the pygame module
+import pygame
+  
+# Define the background colour
+# using RGB color coding.
+background_colour = (234, 212, 252)
+  
+# Define the dimensions of
+# screen object(width,height)
+screen = pygame.display.set_mode((300, 300))
+  
+# Set the caption of the screen
+pygame.display.set_caption('Geeksforgeeks')
+  
+# Fill the background colour to the screen
+screen.fill(background_colour)
+  
+# Update the display using flip
+pygame.display.flip()
+  
+# Variable to keep our game loop running
+running = True
+  
+# game loop
+while running:
+    
+# for loop through the event queue  
+    for event in pygame.event.get():
+      
+        # Check for QUIT event      
+        if event.type == pygame.QUIT:
+            running = False
